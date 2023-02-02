7823a917c27242de5496605a54d811ccccc94ea1
Pedro Lacerda <pedrolacerda@github.com>
2023-02-02 19:28:40 -0300

    feat: add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
+    extend: {
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
