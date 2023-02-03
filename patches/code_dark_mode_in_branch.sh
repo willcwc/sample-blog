7da1d4c38da821b43eadcd050c83ca7e8383eda3
Pedro Lacerda <pedrolacerda@github.com>
2023-02-02 23:08:39 -0300

    fix: task script

--- patches/code_dark_mode_in_branch.sh
+++ patches/code_dark_mode_in_branch.sh
@@ -17,6 +17,8 @@ feature_branch_result=`git branch --list ${feature_branch_name}`
 if  [[ -z $feature_branch_exists ]]; then
   echo "Creating new feature branch: ${feature_branch_name}"
   git checkout -b ${feature_branch_name}
+  current_branch=`git rev-parse --abbrev-ref HEAD`
+  echo "Current Branch: $current_branch"
 else
   echo "Feature branch '${feature_branch_name}' already exists, please delete it before trying again..."
   exit 2
